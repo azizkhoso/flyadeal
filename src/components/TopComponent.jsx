373b3de4f93bae64eb59ae41bc7624d911537aa3
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2022-01-04 00:51:32 +0500

    Added raleway font

--- src/components/TopComponent.jsx
+++ src/components/TopComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import banner from '../assets/Banner.png';
-import logo from '../assets/logo.png';
+import logo from '../assets/logofly.svg';
 import bannerArrow from '../assets/banner arrow.png';
 
 export default function TopComponent() {
@@ -10,7 +10,7 @@ export default function TopComponent() {
       className="w-full px-4 overflow-hidden bg-no-repeat md:px-0"
       style={{ minHeight: 'calc(100vw / 2.5)', backgroundImage: `url(${banner})`, backgroundSize: '100% auto' }}
     >
-      <img src={logo} alt="logo" className="relative w-20 md:w-24 top-4 right-5" style={{ left: 'calc(100vw - 106px)' }} />
+      <img src={logo} alt="logo" className="absolute top-0 right-0" style={{ width: '20%' }} />
       <div className="w-full mx-auto md:w-11/12 xl:8/12" style={{ marginTop: '5%' }}>
         <img src={bannerArrow} alt="arrow" className="" style={{ width: '7%', minWidth: '30px' }} />
         <div className="flex flex-col w-7/12 gap-2 mt-3 md:gap-4 h-3/4">
@@ -18,7 +18,7 @@ export default function TopComponent() {
             . . . . . . . ... .. . . <br />
             . .. . . . . . ... . . . . <br />
           </div>
-          <span className="mt-3 text-white text-mini md:mt-8 sm:text-sm md:text-lg">
+          <span className="mt-3 text-white text-mini md:mt-8 sm:text-sm md:text-lg" style={{ fontFamily: 'raleway' }}>
             Starting Janury 1st 2019, we have found <br />
             cheaper ways for you to fly. *
           </span>
